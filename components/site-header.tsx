90f8da90f34a110615937f13893d5750547e5c27
Sayantandas98 <sayantandas1998@gmail.com>
2023-07-06 23:35:38 +0530

    turnary

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -6,16 +6,17 @@ import { Icons } from "@/components/icons"
 import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
 import { LoginButton, LogoutButton } from "./loginButton"
-
-export function SiteHeader() {
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+export async function SiteHeader() {
+  const sss=await getServerSession(authOptions)
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
-            <LoginButton/>
-            <LogoutButton/>
+            {sss==null?<LoginButton/>:<LogoutButton/>}
             <Link
               href={siteConfig.links.github}
               target="_blank"
